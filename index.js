122be2a04ba86b0b85a282cb62223831ca07d66a
pakistanstarofficial92-sys <pakistanstarofficial92@gmail.com>
2025-12-10 16:30:54 +0500

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("✅ Pakistan Star AI Tool Backend is Running!");
+  res.end("✅ Pakistan Star AI Tool Backend is Running");
 });
 
 server.listen(PORT, () => {
