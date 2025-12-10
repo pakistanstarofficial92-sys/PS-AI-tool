b024efc0e69cd45f9cb772df3fbf2957dd44b51c
pakistanstarofficial92-sys <pakistanstarofficial92@gmail.com>
2025-12-10 16:36:06 +0500

    Update index.js

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
+  console.log(`Server running on port ${PORT}`);
 });
