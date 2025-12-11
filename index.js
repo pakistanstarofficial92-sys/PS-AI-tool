8aba6a57349bf0165b31385b2e68574d96e0e7a6
pakistanstarofficial92-sys <pakistanstarofficial92@gmail.com>
2025-12-11 13:25:04 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
-const express = require("express");
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+import authRoutes from "./src/routes/auth.js";
+import apiRoutes from "./src/routes/api.js";
+
+dotenv.config();
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
+// DB CONNECT
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log("DB Error:", err));
+
+// ROUTES
+app.use("/auth", authRoutes);
+app.use("/api", apiRoutes);
+
 app.get("/", (req, res) => {
-  res.send("PS AI Tool Backend Running");
+  res.send("PS-AI Tool Backend Running");
 });
 
-app.post("/register", (req, res) => {
-  const { email, password } = req.body;
-
-  if (!email || !password) {
-    return res.status(400).json({
-      message: "Email aur password chahiye"
-    });
-  }
-
-  res.json({
-    message: "Register OK",
-    email
-  });
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
-});
+app.listen(process.env.PORT || 5000, () =>
+  console.log(`Server running on port ${process.env.PORT}`)
+);
